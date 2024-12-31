10c520263a8e2e8f12de239dc4def1092870ea85
pnkjsaini <80576375+pnkjsaini@users.noreply.github.com>
2024-12-31 21:02:52 +0530

    Update loop_dir.py

--- loop_dir.py
+++ loop_dir.py
@@ -2,7 +2,7 @@ response_dir={
         'play song': 'which song you like to listen',
         'how are you': ' Bored,but focusing on my goals',
         'have a good day':'Have a nice day sir',
-        'is ekta beautiful':'Sometime feels so'
+        
         }
 
 for key,value in response_dir.items():
